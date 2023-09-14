0084576771da7f454b70247d1bf747a651358a71
Shaun Vivian Dsouza <shaunvividsz@gmail.com>
2023-09-14 01:35:00 +0100

    v2

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,9 +12,9 @@ function Navbar() {
         <img className="navbar-logo_img" src={Logo} alt="Logo" />
       </div>
       <ul className="navbar-nav">
-      <Link to="/housing">  <li className={location.pathname=="/housing"?"navbar-nav_item active":"navbar-nav_item"}>Housing</li></Link>
-        <Link to="/health"><li className={location.pathname=="/health"?"navbar-nav_item active":"navbar-nav_item"}>Health</li></Link>
-       <Link to="/income"> <li className={location.pathname=="/income"?"navbar-nav_item active":"navbar-nav_item"}>Income And poverty</li></Link>
+      <Link to="/housing">  <li className={location.pathname==="/housing"?"navbar-nav_item active":"navbar-nav_item"}>Housing</li></Link>
+        <Link to="/health"><li className={location.pathname==="/health"?"navbar-nav_item active":"navbar-nav_item"}>Health</li></Link>
+       <Link to="/income"> <li className={location.pathname==="/income"?"navbar-nav_item active":"navbar-nav_item"}>Income And poverty</li></Link>
       </ul>
     </nav>
   );
