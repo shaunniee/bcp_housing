e4df9eaa760f6bea23b091d735e679d26ed5898b
Shaun Vivian Dsouza <shaunvividsz@gmail.com>
2023-09-14 09:40:23 +0100

    v8

--- src/Pages/Income/Income.jsx
+++ src/Pages/Income/Income.jsx
@@ -4,7 +4,7 @@ import { TableauEmbed } from "@stoddabr/react-tableau-embed-live";
 function Income() {
   return (
     <div style={{height:"100%"}}>
-    <TableauEmbed sourceUrl="https://public.tableau.com/views/AgeFriendlyElderlyDemographics/IrelandSnapshot?:language=en-US&:display_count=n&:origin=viz_share_link"/>
+    <TableauEmbed sourceUrl="https://public.tableau.com/views/AgeFriendlyElderlyDemographics/IrelandSnapshot?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"/>
     </div>
   )
 }
